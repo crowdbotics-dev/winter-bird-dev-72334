f38b8bd7221aaaa96d87f562a47680687882437e
Crowdbotics <engineering@crowdbotics.com>
2023-03-27 06:36:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "winter-bird-dev-72334",
+  url: "https://winter-bird-dev-72334.botics.co",
+  api: "https://winter-bird-dev-72334.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
